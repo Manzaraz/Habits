61641d6998e71838334d2cf1ebb2175112cce849
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-07-27 22:54:17 -0300

    Habit Model

---
+++ Habits/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Habits
+//
+//  Created by Christian Manzaraz on 27/07/2023.
+//
+
+import Foundation
+struct Color {
+    
+    let hue: Double
+    let saturation: Double
+    let brightness: Double
+    
+}
---
+++ Habits/Habit.swift
@@ -0,0 +1,17 @@
+//
+//  Habit.swift
+//  Habits
+//
+//  Created by Christian Manzaraz on 27/07/2023.
+//
+
+import Foundation
+
+struct Habit {
+    
+    let name: String
+    let category: Category
+    let info: Stream
+    
+    
+}
