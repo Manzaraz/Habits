4c98cf1a72a4ff7d125bfe423685dcc7306673f5
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-07-30 22:33:00 -0300

    Create a Model and View Controller

--- Habits/Habit.swift
+++ Habits/Habit.swift
@@ -11,8 +11,19 @@ struct Habit {
     let name: String
     let category: Category
     let info: String
+    
 }
 
 extension Habit: Codable {
+    
+}
+extension Habit: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+    
+    static func == (lhs: Habit, rhs: Habit) -> Bool {
+        return lhs.name == rhs.name
+    }
     
 }
