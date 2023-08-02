069e9ed3ebf7f4620a7c58e92345bb8553035311
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-08-02 18:49:25 -0300

    Set Up Static View Information

--- Habits/HabitDetailViewController.swift
+++ Habits/HabitDetailViewController.swift
@@ -14,12 +14,23 @@ class HabitDetailViewController: UIViewController {
     @IBOutlet var infoLabel: UILabel!
     @IBOutlet var collectionView: UICollectionView!
         
+    var habit: Habit!
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init?(coder: NSCoder, habit: Habit) {
+        self.habit = habit
+        super.init(coder: coder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameLabel.text = habit.name
+        categoryLabel.text = habit.category.name
+        infoLabel.text = habit.info
         
     }
 
