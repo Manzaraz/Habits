027170188de336c1a0653be4a92bf4b0928bb050
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-08-03 15:16:48 -0300

    Set up the Person Detail Scene

--- Habits/UserDetailViewController.swift
+++ Habits/UserDetailViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class UserDetailViewController: UIViewController {
-
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var bioLabel: UILabel!
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
