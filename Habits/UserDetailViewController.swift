4d11513e71e8a1d66ed024cb5aaa7871fd537f22
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-08-03 15:17:27 -0300

    Set Up Static View Information

--- Habits/UserDetailViewController.swift
+++ Habits/UserDetailViewController.swift
@@ -14,10 +14,25 @@ class UserDetailViewController: UIViewController {
     @IBOutlet var bioLabel: UILabel!
     @IBOutlet var collectionView: UICollectionView!
     
+    var user: User!
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init?(coder: NSCoder, user: User) {
+        self.user = user
+        super.init(coder: coder)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        userNameLabel.text = user.name
+        bioLabel.text = user.bio
+        
     }
     
 
